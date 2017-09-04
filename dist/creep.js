d5ed58402080d5a89906a48ed03f428d95f6657f
tereshchenkomax <tereshchenkomax@gmail.com>
2017-09-04 20:03:03 +0300

    Ubuntu

---
+++ dist/creep.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class creep extends Component {
+
+}
+
+creep.propTypes = {};
+creep.defaultProps = {};
+
+export default creep;
+
